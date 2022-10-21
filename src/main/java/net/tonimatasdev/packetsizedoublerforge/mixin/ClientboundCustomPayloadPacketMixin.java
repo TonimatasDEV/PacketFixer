1b4f214b4d9a38a6d2025cf078e9a89721d443c1
TonimatasDEV <79017964+TonimatasDEV@users.noreply.github.com>
2022-10-21 18:30:43 +0200

    Upload ClientboundCustomPayloadPacketMixin.java

---
+++ src/main/java/net/tonimatasdev/packetsizedoublerforge/mixin/ClientboundCustomPayloadPacketMixin.java
@@ -0,0 +1,19 @@
+package net.tonimatasdev.packetsizedoublerforge.mixin;
+
+import net.minecraft.network.protocol.game.ClientboundCustomPayloadPacket;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(ClientboundCustomPayloadPacket.class)
+public class ClientboundCustomPayloadPacketMixin {
+    @ModifyConstant(method = "<init>(Lnet/minecraft/resources/ResourceLocation;Lnet/minecraft/network/FriendlyByteBuf;)V",constant = @Constant(intValue = 1048576))
+    private int injectedRF(int value) {
+        return value*10;
+    }
+
+    @ModifyConstant(method = "<init>(Lnet/minecraft/network/FriendlyByteBuf;)V",constant = @Constant(intValue = 1048576))
+    private int injectedF(int value) {
+        return value*10;
+    }
+}
