bd0658e5b9461d1e203457d2869a954478005cb7
TonimatasDEV <79017964+TonimatasDEV@users.noreply.github.com>
2022-10-21 20:57:31 +0200

    More time for login

---
+++ src/main/java/net/tonimatasdev/packetsizedoublerforge/mixin/ServerLoginPacketListenerImplMixin.java
@@ -0,0 +1,16 @@
+package net.tonimatasdev.packetsizedoublerforge.mixin;
+
+import net.minecraft.server.network.ServerLoginPacketListenerImpl;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(ServerLoginPacketListenerImpl.class)
+
+public class ServerLoginPacketListenerImplMixin {
+
+    @ModifyConstant(method = "tick", constant = @Constant(intValue = 600))
+    private int injected(int value) {
+        return value*10;
+    }
+}
