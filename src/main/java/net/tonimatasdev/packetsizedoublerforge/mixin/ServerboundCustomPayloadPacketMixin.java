8b843f9904534251c14b6ef35fcb94580197b3e4
TonimatasDEV <79017964+TonimatasDEV@users.noreply.github.com>
2022-10-21 19:03:50 +0200

    Fix more errors

---
+++ src/main/java/net/tonimatasdev/packetsizedoublerforge/mixin/ServerboundCustomPayloadPacketMixin.java
@@ -0,0 +1,15 @@
+package net.tonimatasdev.packetsizedoublerforge.mixin;
+
+import net.minecraft.network.protocol.game.ServerboundCustomPayloadPacket;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(ServerboundCustomPayloadPacket.class)
+public class ServerboundCustomPayloadPacketMixin {
+
+    @ModifyConstant(method = "<init>(Lnet/minecraft/network/FriendlyByteBuf;)V", constant = @Constant(intValue = 32767))
+    private int injected(int value) {
+        return value*10;
+    }
+}
