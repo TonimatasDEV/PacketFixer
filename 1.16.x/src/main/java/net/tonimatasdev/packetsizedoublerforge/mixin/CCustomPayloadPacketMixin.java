43a50ff426d6e1e73086b061d001c12f6643fddb
TonimatasMC <amatascubero@gmail.com>
2022-10-22 16:11:48 +0200

    Port [1.16.x, 1.17.x, 1.18.x, 1.19.x]

---
+++ 1.16.x/src/main/java/net/tonimatasdev/packetsizedoublerforge/mixin/CCustomPayloadPacketMixin.java
@@ -0,0 +1,15 @@
+package net.tonimatasdev.packetsizedoublerforge.mixin;
+
+import net.minecraft.network.play.client.CCustomPayloadPacket;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(CCustomPayloadPacket.class)
+public class CCustomPayloadPacketMixin {
+
+    @ModifyConstant(method = "read", constant = @Constant(intValue = 32767))
+    private int injected(int value) {
+        return value*10;
+    }
+}
