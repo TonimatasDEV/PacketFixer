1792ca5f856bc64685eb8bbcfee3e345a46ca881
TonimatasDEV <79017964+TonimatasDEV@users.noreply.github.com>
2022-12-03 22:10:31 +0100

    1.0.3

--- 1.15.x/src/main/java/net/tonimatasdev/packetsizedoublerforge/mixin/NettyCompressionDecoderMixin.java
+++ 1.15.x/src/main/java/net/tonimatasdev/packetsizedoublerforge/mixin/NettyCompressionDecoderMixin.java
@@ -6,7 +6,7 @@ import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Mixin({NettyCompressionDecoder.class})
-public class CompressionDecoderMixin {
+public class NettyCompressionDecoderMixin {
 
     @ModifyConstant(method = "decode", constant = @Constant(intValue = 8388608))
     private int injected(int value) {
--- 1.16.x/src/main/java/net/tonimatasdev/packetsizedoublerforge/mixin/NettyCompressionEncoderMixin.java
+++ 1.16.x/src/main/java/net/tonimatasdev/packetsizedoublerforge/mixin/NettyCompressionEncoderMixin.java
@@ -6,7 +6,7 @@ import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Mixin({NettyCompressionEncoder.class})
-public class CompressionEncoderMixin {
+public class NettyCompressionEncoderMixin {
 
     @ModifyConstant(method = "encode(Lio/netty/channel/ChannelHandlerContext;Lio/netty/buffer/ByteBuf;Lio/netty/buffer/ByteBuf;)V", constant = @Constant(intValue = 2097152))
     private int injected(int value) {
