cbcbdade35817742c4ec686c072d9dda1aed0279
TonimatasDEV <amatascubero@gmail.com>
2023-06-27 21:58:33 +0200

    require is the solution?

--- src/main/java/net/tonimatasdev/packetsizedoubler/mixin/PacketEncoderMixin.java
+++ src/main/java/net/tonimatasdev/packetsizedoubler/mixin/PacketEncoderMixin.java
@@ -7,7 +7,7 @@ import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Mixin(PacketEncoder.class)
 public class PacketEncoderMixin {
-    @ModifyConstant(method = "encode(Lio/netty/channel/ChannelHandlerContext;Lnet/minecraft/network/protocol/Packet;Lio/netty/buffer/ByteBuf;)V", constant = @Constant(intValue = 8388608))
+    @ModifyConstant(method = "encode(Lio/netty/channel/ChannelHandlerContext;Lnet/minecraft/network/protocol/Packet;Lio/netty/buffer/ByteBuf;)V", constant = @Constant(intValue = 8388608), require = 0)
     private int packetDoubler(int value) {
         return value * 100;
     }
