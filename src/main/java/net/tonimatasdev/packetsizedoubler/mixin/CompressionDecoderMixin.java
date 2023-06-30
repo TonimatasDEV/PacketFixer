1781788bb52635cdcf8c7085c6ea977fc505a680
TonimatasDEV <amatascubero@gmail.com>
2023-07-01 01:10:19 +0200

    Fix Connectivity compatibility

--- src/main/java/net/tonimatasdev/packetsizedoubler/mixin/CompressionDecoderMixin.java
+++ src/main/java/net/tonimatasdev/packetsizedoubler/mixin/CompressionDecoderMixin.java
@@ -5,7 +5,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
-@Mixin({CompressionDecoder.class})
+@Mixin(value = CompressionDecoder.class, priority = 999)
 public class CompressionDecoderMixin {
     @ModifyConstant(method = "decode", constant = @Constant(intValue = 8388608), require = 0)
     private int packetDoubler(int value) {
--- src/main/java/net/tonimatasdev/packetsizedoubler/mixin/CompressionEncoderMixin.java
+++ src/main/java/net/tonimatasdev/packetsizedoubler/mixin/CompressionEncoderMixin.java
@@ -5,7 +5,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
-@Mixin({CompressionEncoder.class})
+@Mixin(value = CompressionEncoder.class, priority = 999)
 public class CompressionEncoderMixin {
     @ModifyConstant(method = "encode(Lio/netty/channel/ChannelHandlerContext;Lio/netty/buffer/ByteBuf;Lio/netty/buffer/ByteBuf;)V", constant = @Constant(intValue = 2097152), require = 0)
     private int packetDoubler(int value) {
