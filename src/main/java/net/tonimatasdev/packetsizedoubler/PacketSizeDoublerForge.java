b932f5d261e0c120f126520e851cc5c05a0302af
TonimatasDEV <79017964+TonimatasDEV@users.noreply.github.com>
2023-07-01 21:18:51 +0200

    mmm Fabric?

---
+++ src/main/java/net/tonimatasdev/packetsizedoubler/PacketSizeDoublerForge.java
@@ -0,0 +1,11 @@
+package net.tonimatasdev.packetsizedoubler;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("packetsizedoubler")
+public class PacketSizeDoublerForge {
+    public PacketSizeDoublerForge() {
+        LogUtils.getLogger().info("PacketSizeDoubler (Forge) has been initialized successfully");
+    }
+}
