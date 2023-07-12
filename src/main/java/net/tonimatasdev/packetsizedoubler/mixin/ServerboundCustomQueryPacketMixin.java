21c54615a05483bc2a2a593b55e6ab8d3af7ab78
TonimatasDEV <amatascubero@gmail.com>
2023-07-12 18:40:11 +0200

    Add logo and fixes

--- src/main/java/net/tonimatasdev/packetsizedoubler/mixin/ServerboundCustomQueryPacketMixin.java
+++ src/main/java/net/tonimatasdev/packetsizedoubler/mixin/ServerboundCustomQueryPacketMixin.java
@@ -7,7 +7,7 @@ import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Mixin(value = ServerboundCustomQueryPacket.class, priority = 999)
 public class ServerboundCustomQueryPacketMixin {
-    @ModifyConstant(method = "<init>(Lnet/minecraft/network/FriendlyByteBuf;)V", constant = @Constant(intValue = 1048576))
+    @ModifyConstant(method = "<init>(Lnet/minecraft/network/FriendlyByteBuf;)V", constant = @Constant(intValue = 1048576), require = 0)
     private int newSize(int value) {
         return value * 100;
     }
