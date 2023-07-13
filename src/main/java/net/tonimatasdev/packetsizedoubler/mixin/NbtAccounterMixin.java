1f9550189d01c438f35fa752f249a776cc1ee8d6
TonimatasDEV <amatascubero@gmail.com>
2023-07-13 16:50:21 +0200

    Fix many things

--- src/main/java/net/tonimatasdev/packetsizedoubler/mixin/NbtAccounterMixin.java
+++ src/main/java/net/tonimatasdev/packetsizedoubler/mixin/NbtAccounterMixin.java
@@ -1,6 +1,5 @@
 package net.tonimatasdev.packetsizedoubler.mixin;
 
-import com.mojang.logging.LogUtils;
 import net.minecraft.nbt.NbtAccounter;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
@@ -8,10 +7,9 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(value = NbtAccounter.class, priority = 999)
-public class NbtAccounterMixin {
-    @Redirect(method = "accountBytes", at = @At(value = "FIELD", target = "Lnet/minecraft/nbt/NbtAccounter;quota:J", opcode = Opcodes.GETFIELD))
-    private long newSize(NbtAccounter instance) {
-        LogUtils.getLogger().info(String.valueOf(2097152L * 1000));
-        return 2097152L * 1000;
+public abstract class NbtAccounterMixin {
+    @Redirect(method = "accountBits", at = @At(value = "FIELD", target = "Lnet/minecraft/nbt/NbtAccounter;quota:J", opcode = Opcodes.GETFIELD))
+    public long accountBits(NbtAccounter instance) {
+        return 2097152 * 100;
     }
 }
