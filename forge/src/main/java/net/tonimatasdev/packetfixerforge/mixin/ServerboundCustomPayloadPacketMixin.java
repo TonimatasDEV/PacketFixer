c32ea46242a75110372e7660e57c91ce78182849
TonimatasDEV <amatascubero@gmail.com>
2023-09-07 15:53:14 +0200

    Connectivity compatibility

--- forge/src/main/java/net/tonimatasdev/packetfixerforge/mixin/ServerboundCustomPayloadPacketMixin.java
+++ forge/src/main/java/net/tonimatasdev/packetfixerforge/mixin/ServerboundCustomPayloadPacketMixin.java
@@ -5,7 +5,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
-@Mixin(value = ServerboundCustomPayloadPacket.class, priority = 999)
+@Mixin(value = ServerboundCustomPayloadPacket.class, priority = 9999)
 public class ServerboundCustomPayloadPacketMixin {
     @ModifyConstant(method = "<init>(Lnet/minecraft/network/FriendlyByteBuf;)V", constant = @Constant(intValue = 32767))
     private int newSize(int value) {
