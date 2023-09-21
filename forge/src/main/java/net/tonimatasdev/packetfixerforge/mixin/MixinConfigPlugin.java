539290bdd074aa703b3a16ab2b45bcb47695d441
TonimatasDEV <79017964+TonimatasDEV@users.noreply.github.com>
2023-09-21 23:31:37 +0200

    Delete unnecessary files

--- forge/src/main/java/net/tonimatasdev/packetfixerforge/mixin/MixinConfigPlugin.java
+++ forge/src/main/java/net/tonimatasdev/packetfixerforge/mixin/MixinConfigPlugin.java
@@ -23,12 +23,9 @@ public class MixinConfigPlugin implements IMixinConfigPlugin {
     @Override
     public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
         boolean connectivity = FMLLoader.getLoadingModList().getModFileById("connectivity") != null;
-        boolean immersivePortals = FMLLoader.getLoadingModList().getModFileById("immersive_portals") != null;
 
         if (mixinClassName.equalsIgnoreCase("net.tonimatasdev.packetfixerforge.mixin.CompressionDecoderMixin")) return !connectivity;
         if (mixinClassName.equalsIgnoreCase("net.tonimatasdev.packetfixerforge.mixin.compat.connectivity.CompressionDecoderMixin")) return connectivity;
-        if (mixinClassName.equalsIgnoreCase("net.tonimatasdev.packetfixerforge.mixin.ClientboundCustomPayloadPacketMixin")) return !immersivePortals;
-        if (mixinClassName.equalsIgnoreCase("net.tonimatasdev.packetfixerforge.mixin.compat.immersiveportals.ClientboundCustomPayloadPacketMixin")) return immersivePortals;
 
         return true;
     }
