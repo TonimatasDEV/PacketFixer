b681885c1b0f1c2e7c585606f56352cda7e9638a
TonimatasDEV <79017964+TonimatasDEV@users.noreply.github.com>
2023-10-31 00:37:13 +0100

    1.20.1

--- fabric/src/main/java/net/tonimatasdev/packetfixerfabric/mixin/CustomPayloadC2SPacketMixin.java
+++ fabric/src/main/java/net/tonimatasdev/packetfixerfabric/mixin/CustomPayloadC2SPacketMixin.java
@@ -1,15 +1,22 @@
 package net.tonimatasdev.packetfixerfabric.mixin;
 
 
-import net.minecraft.network.packet.c2s.common.CustomPayloadC2SPacket;
+import net.minecraft.network.packet.c2s.play.CustomPayloadC2SPacket;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Mixin(value = CustomPayloadC2SPacket.class, priority = 9999)
 public class CustomPayloadC2SPacketMixin {
-    @ModifyConstant(method = "readUnknownPayload", constant = @Constant(intValue = 32767))
+    // <= 1.20.1
+    @ModifyConstant(method = "<init>(Lnet/minecraft/network/PacketByteBuf;)V", constant = @Constant(intValue = 32767))
     private static int newSize(int value) {
         return Integer.MAX_VALUE;
     }
+
+    // 1.20.2
+    //@ModifyConstant(method = "readUnknownPayload", constant = @Constant(intValue = 32767))
+    //private static int newSize(int value) {
+    //    return Integer.MAX_VALUE;
+    //}
 }
