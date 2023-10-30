c10a649a83955357cd8c929e07cd95b7bd77533a
TonimatasDEV <79017964+TonimatasDEV@users.noreply.github.com>
2023-10-31 00:28:25 +0100

    Fix cant fit X into 3 error when joining a server

--- forge/src/main/java/net/tonimatasdev/packetfixerforge/mixin/ServerboundCustomQueryPacketMixin.java
+++ forge/src/main/java/net/tonimatasdev/packetfixerforge/mixin/ServerboundCustomQueryPacketMixin.java
@@ -1,11 +1,12 @@
 package net.tonimatasdev.packetfixerforge.mixin;
 
-import net.minecraft.network.protocol.login.ServerboundCustomQueryAnswerPacket;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.network.protocol.login.ServerboundCustomQueryPacket;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
-@Mixin(value = ServerboundCustomQueryAnswerPacket.class, priority = 9999)
+@Mixin(value = ServerboundCustomQueryPacket.class, priority = 9999)
 public class ServerboundCustomQueryPacketMixin {
     // <= 1.18
     //@ModifyConstant(method = "<init>(Lnet/minecraft/network/FriendlyByteBuf;)V", constant = @Constant(intValue = 1048576))
@@ -14,14 +15,14 @@ public class ServerboundCustomQueryPacketMixin {
     //}
 
     // 1.19 to 1.20.1
-    //@ModifyVariable(method = "<init>(Lnet/minecraft/network/FriendlyByteBuf;)V", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/network/FriendlyByteBuf;readNullable(Lnet/minecraft/network/FriendlyByteBuf$Reader;)Ljava/lang/Object;"), index = 1, argsOnly = true)
-    //private FriendlyByteBuf redirectReadNullable(FriendlyByteBuf value) {
-    //    return new FriendlyByteBuf(value.readBytes(value.readableBytes()));
-    //}
+    @ModifyVariable(method = "<init>(Lnet/minecraft/network/FriendlyByteBuf;)V", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/network/FriendlyByteBuf;readNullable(Lnet/minecraft/network/FriendlyByteBuf$Reader;)Ljava/lang/Object;"), index = 1, argsOnly = true)
+    private FriendlyByteBuf redirectReadNullable(FriendlyByteBuf value) {
+        return new FriendlyByteBuf(value.readBytes(value.readableBytes()));
+    }
 
     // >= 1.20.2
-    @ModifyConstant(method = "readUnknownPayload", constant = @Constant(intValue = 1048576))
-    private static int newSize(int value) {
-        return Integer.MAX_VALUE;
-    }
+    //@ModifyConstant(method = "readUnknownPayload", constant = @Constant(intValue = 1048576))
+    //private static int newSize(int value) {
+    //    return Integer.MAX_VALUE;
+    //}
 }
