e860cfb3b90e20fac25b4c6b8629bccc3c330a34
TonimatasDEV <79017964+TonimatasDEV@users.noreply.github.com>
2023-11-06 19:29:28 +0100

    Fix compatibility with Krypton

--- fabric/src/main/java/net/tonimatasdev/packetfixerfabric/mixin/MixinConfigPlugin.java
+++ fabric/src/main/java/net/tonimatasdev/packetfixerfabric/mixin/MixinConfigPlugin.java
@@ -1,5 +1,6 @@
 package net.tonimatasdev.packetfixerfabric.mixin;
 
+import com.mojang.logging.LogUtils;
 import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.ModContainer;
 import net.fabricmc.loader.api.metadata.ModMetadata;
@@ -26,12 +27,14 @@ public class MixinConfigPlugin implements IMixinConfigPlugin {
         List<String> mods = FabricLoader.getInstance().getAllMods().stream().map(ModContainer::getMetadata).map(ModMetadata::getId).toList();
 
         boolean connectivity = mods.contains("connectivity");
-        boolean immersivePortals = mods.contains("immersive_portals");
+        boolean krypton = mods.contains("krypton");
 
         if (mixinClassName.equalsIgnoreCase("net.tonimatasdev.packetfixerfabric.mixin.PacketInflaterMixin")) return !connectivity;
         if (mixinClassName.equalsIgnoreCase("net.tonimatasdev.packetfixerfabric.mixin.compat.connectivity.PacketInflaterMixin")) return connectivity;
-        if (mixinClassName.equalsIgnoreCase("net.tonimatasdev.packetfixerfabric.mixin.CustomPayloadS2CPacket")) return !immersivePortals;
-        if (mixinClassName.equalsIgnoreCase("net.tonimatasdev.packetfixerfabric.mixin,immersiveportals.CustomPayloadS2CPacket")) return immersivePortals;
+        if (mixinClassName.equalsIgnoreCase("net.tonimatasdev.packetfixerfabric.mixin.SplitterHandlerMixin") || mixinClassName.equalsIgnoreCase("net.tonimatasdev.packetfixerfabric.mixin.SizePrependerMixin")) {
+            LogUtils.getLogger().warn("For can't fit X into 3 error fix. Delete Krypton.");
+            return !krypton;
+        }
 
         return true;
     }
