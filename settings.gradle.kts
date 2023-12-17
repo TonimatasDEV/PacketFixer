7b356ac81917fbdae7eabeb57e541ff14edae6b5
TonimatasDEV <amatascubero@gmail.com>
2023-12-17 17:14:21 +0100

    Rebuild wrapper

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,9 @@
 pluginManagement {
     repositories {
-        mavenCentral()
         gradlePluginPortal()
         maven(url = "https://maven.minecraftforge.net/")
         maven(url = "https://maven.architectury.dev/")
         maven(url = "https://maven.fabricmc.net/")
-        maven(url = "https://maven.neoforged.net/releases")
     }
 }
 
