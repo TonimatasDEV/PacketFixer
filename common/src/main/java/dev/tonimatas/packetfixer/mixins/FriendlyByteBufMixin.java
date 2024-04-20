3fb333bcca1a54d5b49aedd6dc1f8e6172af6c9e
TonimatasDEV <79017964+TonimatasDEV@users.noreply.github.com>
2024-04-20 19:37:52 +0200

    Change class name

--- common/src/main/java/dev/tonimatas/packetfixer/mixins/FriendlyByteBufMixin.java
+++ common/src/main/java/dev/tonimatas/packetfixer/mixins/FriendlyByteBufMixin.java
@@ -6,7 +6,7 @@ import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Mixin(value = FriendlyByteBuf.class, priority = 9999)
-public abstract class NbtAccounterMixin {
+public abstract class FriendlyByteBufMixin {
     @ModifyConstant(method = "readNbt()Lnet/minecraft/nbt/CompoundTag;", constant = @Constant(longValue = 2097152L))
     public long newSize(long value) {
         return Long.MAX_VALUE;
