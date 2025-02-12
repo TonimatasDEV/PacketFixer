3d26ee3c75ac98cd53ca0c791da4f8805c4d14a9
tonimatasdev <me@tonimatas.dev>
2025-02-12 16:01:09 +0100

    Fix compatibility problems with Krypton

--- common/src/main/java/dev/tonimatas/packetfixer/mixins/VarIntMixin.java
+++ common/src/main/java/dev/tonimatas/packetfixer/mixins/VarIntMixin.java
@@ -6,7 +6,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
-@Mixin(VarInt.class)
+@Mixin(value = VarInt.class, priority = 1001)
 public class VarIntMixin {
     @ModifyConstant(method = "getByteSize", constant = @Constant(intValue = 5))
     private static int getByteSize$size(int value) {
