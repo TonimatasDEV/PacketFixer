f9747613f064ea1f634d46544eb471e97fbf1d7f
tonimatasdev <me@tonimatas.dev>
2025-02-14 18:28:59 +0100

    It works fine now

--- common/src/main/java/dev/tonimatas/packetfixer/mixins/Varint21FrameDecoderMixin.java
+++ common/src/main/java/dev/tonimatas/packetfixer/mixins/Varint21FrameDecoderMixin.java
@@ -2,6 +2,7 @@ package dev.tonimatas.packetfixer.mixins;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
 import net.minecraft.network.VarInt;
 import net.minecraft.network.Varint21FrameDecoder;
 import org.spongepowered.asm.mixin.Final;
@@ -9,16 +10,17 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.List;
 
 @Mixin(Varint21FrameDecoder.class)
 public abstract class Varint21FrameDecoderMixin {
     @Mutable @Shadow @Final private ByteBuf helperBuf;
 
-    @Redirect(method = "decode", at = @At(value = "INVOKE", target = "Lio/netty/buffer/ByteBuf;markReaderIndex()Lio/netty/buffer/ByteBuf;"))
-    private ByteBuf init$helper(ByteBuf instance) {
-        instance.markReaderIndex();
-        this.helperBuf = Unpooled.directBuffer(VarInt.getByteSize(instance.readableBytes()) + 2);
-        return instance;
+    @Inject(method = "decode", at = @At("HEAD"))
+    private void init$helper(ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf, List<Object> list, CallbackInfo ci) {
+        this.helperBuf = Unpooled.directBuffer(VarInt.getByteSize(byteBuf.readableBytes()) + 2);
     }
 
     @ModifyConstant(method = "copyVarint", constant = @Constant(intValue = 3))
