4b055d29eeb98083a08bf50cae3bb8d6b3806dfc
tonimatasdev <me@tonimatas.dev>
2025-02-14 18:00:26 +0100

    Fix memory problems

--- common/src/main/java/dev/tonimatas/packetfixer/mixins/Varint21FrameDecoderMixin.java
+++ common/src/main/java/dev/tonimatas/packetfixer/mixins/Varint21FrameDecoderMixin.java
@@ -1,31 +1,28 @@
 package dev.tonimatas.packetfixer.mixins;
 
-import dev.tonimatas.packetfixer.util.Config;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
-import net.minecraft.network.BandwidthDebugMonitor;
+import net.minecraft.network.VarInt;
 import net.minecraft.network.Varint21FrameDecoder;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.*;
 
 @Mixin(Varint21FrameDecoder.class)
 public abstract class Varint21FrameDecoderMixin {
     @Mutable @Shadow @Final private ByteBuf helperBuf;
 
-    @Inject(method = "<init>", at = @At("RETURN"))
-    private void init$helper(BandwidthDebugMonitor bandwidthDebugMonitor, CallbackInfo ci) {
-        this.helperBuf = Unpooled.directBuffer(Config.getVarInt21Size());
+    @Redirect(method = "decode", at = @At(value = "INVOKE", target = "Lio/netty/buffer/ByteBuf;markReaderIndex()Lio/netty/buffer/ByteBuf;"))
+    private ByteBuf init$helper(ByteBuf instance) {
+        instance.markReaderIndex();
+        this.helperBuf = Unpooled.directBuffer(VarInt.getByteSize(instance.readableBytes()) + 2);
+        return instance;
     }
 
     @ModifyConstant(method = "copyVarint", constant = @Constant(intValue = 3))
-    private static int newSize(int value) {
-        return Config.getVarInt21Size();
+    private static int newSize(int value, ByteBuf byteBuf) {
+        return VarInt.getByteSize(byteBuf.readableBytes()) + 2;
     }
 }
