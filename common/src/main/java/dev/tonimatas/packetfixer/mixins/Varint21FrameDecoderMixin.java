5babb7fbdbc13fc9ca8f1d9c9c0f748263176222
TonimatasDEV <79017964+TonimatasDEV@users.noreply.github.com>
2025-02-14 18:40:08 +0100

    Fix memory problems

--- common/src/main/java/dev/tonimatas/packetfixer/mixins/Varint21FrameDecoderMixin.java
+++ common/src/main/java/dev/tonimatas/packetfixer/mixins/Varint21FrameDecoderMixin.java
@@ -1,9 +1,9 @@
 package dev.tonimatas.packetfixer.mixins;
 
-import dev.tonimatas.packetfixer.util.Config;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
-import net.minecraft.network.BandwidthDebugMonitor;
+import io.netty.channel.ChannelHandlerContext;
+import net.minecraft.network.VarInt;
 import net.minecraft.network.Varint21FrameDecoder;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -15,17 +15,19 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.List;
+
 @Mixin(Varint21FrameDecoder.class)
 public abstract class Varint21FrameDecoderMixin {
     @Mutable @Shadow @Final private ByteBuf helperBuf;
 
-    @Inject(method = "<init>", at = @At("RETURN"))
-    private void init$helper(BandwidthDebugMonitor bandwidthDebugMonitor, CallbackInfo ci) {
-        this.helperBuf = Unpooled.directBuffer(Config.getVarInt21Size());
+    @Inject(method = "decode", at = @At("HEAD"))
+    private void init$helper(ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf, List<Object> list, CallbackInfo ci) {
+        this.helperBuf = Unpooled.directBuffer(VarInt.getByteSize(byteBuf.readableBytes()) + 2);
     }
-    
+
     @ModifyConstant(method = "copyVarint", constant = @Constant(intValue = 3))
-    private static int newSize(int original) {
-        return Config.getVarInt21Size();
+    private static int newSize(int value, ByteBuf byteBuf) {
+        return VarInt.getByteSize(byteBuf.readableBytes()) + 2;
     }
 }
