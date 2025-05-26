e49e341bd1ec40ba5d7d54562c792c03ab87f189
tonimatasdev <79017964+TonimatasDEV@users.noreply.github.com>
2025-05-26 21:18:46 +0200

    Fix warning

--- forge/src/main/java/dev/tonimatas/packetfixer/mixins/NettyVarint21FrameDecoderMixin.java
+++ forge/src/main/java/dev/tonimatas/packetfixer/mixins/NettyVarint21FrameDecoderMixin.java
@@ -13,11 +13,11 @@ import org.spongepowered.asm.mixin.injection.ModifyConstant;
 public class NettyVarint21FrameDecoderMixin {
     @ModifyConstant(method = "decode", constant = @Constant(intValue = 3))
     private int newSize$decode(int constant, ChannelHandlerContext p_decode_1_, ByteBuf p_decode_2_) {
-        return getByteSize(p_decode_2_.readableBytes()) + 2;
+        return newSize$getByteSize(p_decode_2_.readableBytes()) + 2;
     }
     
     @Unique
-    private static int getByteSize(int data) {
+    private static int newSize$getByteSize(int data) {
         for(int i = 1; i < Config.getVarIntSize(); ++i) {
             if ((data & -1 << i * 7) == 0) {
                 return i;
