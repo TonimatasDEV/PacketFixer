1ce010fd25f48195de0a6baa89884f5621a7885f
tonimatasdev <79017964+TonimatasDEV@users.noreply.github.com>
2025-05-26 21:18:25 +0200

    Fix #72 and update 2.1.0

--- forge/src/main/java/dev/tonimatas/packetfixer/mixins/NettyVarint21FrameDecoderMixin.java
+++ forge/src/main/java/dev/tonimatas/packetfixer/mixins/NettyVarint21FrameDecoderMixin.java
@@ -1,15 +1,29 @@
 package dev.tonimatas.packetfixer.mixins;
 
 import dev.tonimatas.packetfixer.util.Config;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
 import net.minecraft.network.NettyVarint21FrameDecoder;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Mixin(NettyVarint21FrameDecoder.class)
 public class NettyVarint21FrameDecoderMixin {
     @ModifyConstant(method = "decode", constant = @Constant(intValue = 3))
-    private int newSize$decode(int constant) {
-        return Config.getVarInt21Size();
+    private int newSize$decode(int constant, ChannelHandlerContext p_decode_1_, ByteBuf p_decode_2_) {
+        return getByteSize(p_decode_2_.readableBytes()) + 2;
+    }
+    
+    @Unique
+    private static int getByteSize(int data) {
+        for(int i = 1; i < Config.getVarIntSize(); ++i) {
+            if ((data & -1 << i * 7) == 0) {
+                return i;
+            }
+        }
+
+        return Config.getVarIntSize();
     }
 }
