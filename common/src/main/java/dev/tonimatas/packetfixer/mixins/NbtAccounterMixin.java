a8685800fd1830bb1fe40e705f331a6ab5c2b44e
tonimatasdev <79017964+TonimatasDEV@users.noreply.github.com>
2025-06-10 22:18:04 +0200

    fix: NbtAccounter throw

---
+++ common/src/main/java/dev/tonimatas/packetfixer/mixins/NbtAccounterMixin.java
@@ -0,0 +1,19 @@
+package dev.tonimatas.packetfixer.mixins;
+
+import dev.tonimatas.packetfixer.util.Config;
+import net.minecraft.nbt.NbtAccounter;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(NbtAccounter.class)
+public class NbtAccounterMixin {
+    @Shadow @Final private long quota;
+
+    @Redirect(method = "accountBits", at = @At(value = "FIELD", target = "Lnet/minecraft/nbt/NbtAccounter;quota:J"))
+    private long newSize$accountBits(NbtAccounter instance) {
+        return Config.getNbtMaxSize() < quota ? quota : Config.getNbtMaxSize();
+    }
+}
