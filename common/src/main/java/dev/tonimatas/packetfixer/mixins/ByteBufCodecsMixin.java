e9ed625f978e2ae916cf6c8d49d702c51866e5bb
Juan Patricio <juanmuscaria@gmail.com>
2025-06-11 13:19:20 -0300

    Patch ByteBufCodecs usage of NbtAccounter, fixes PacketFixer#70

---
+++ common/src/main/java/dev/tonimatas/packetfixer/mixins/ByteBufCodecsMixin.java
@@ -0,0 +1,16 @@
+package dev.tonimatas.packetfixer.mixins;
+
+import dev.tonimatas.packetfixer.util.Config;
+import net.minecraft.network.codec.ByteBufCodecs;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(ByteBufCodecs.class)
+public interface ByteBufCodecsMixin {
+    // Target all NbtAccounter lambdas
+    @ModifyConstant(method = "*()Lnet/minecraft/nbt/NbtAccounter;", constant = @Constant(longValue = 2097152L))
+    private static long newSize(long value) {
+        return Config.getNbtMaxSize();
+    }
+}
