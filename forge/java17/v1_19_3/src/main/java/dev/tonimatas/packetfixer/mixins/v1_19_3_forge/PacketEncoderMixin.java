010e0fde649508ef494b9729d3fe353bc324d126
tonimatasdev <79017964+TonimatasDEV@users.noreply.github.com>
2025-07-02 14:19:28 +0200

    fix: Packet too big (is X, should be less than 8388608)

---
+++ forge/java17/v1_19_3/src/main/java/dev/tonimatas/packetfixer/mixins/v1_19_3_forge/PacketEncoderMixin.java
@@ -0,0 +1,15 @@
+package dev.tonimatas.packetfixer.mixins.v1_19_3_forge;
+
+import dev.tonimatas.packetfixer.common.Config;
+import net.minecraft.network.PacketEncoder;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(PacketEncoder.class)
+public class PacketEncoderMixin {
+    @ModifyConstant(method = "encode(Lio/netty/channel/ChannelHandlerContext;Lnet/minecraft/network/protocol/Packet;Lio/netty/buffer/ByteBuf;)V", constant = @Constant(intValue = 8388608))
+    private int packetfixer$newSize(int value) {
+        return Config.getDecoderSize();
+    }
+}
