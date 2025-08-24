30631040a2a240d498f3a5309569ba40f74ecfdf
tonimatasdev <79017964+TonimatasDEV@users.noreply.github.com>
2025-08-24 08:58:46 +0200

    Stop calling properties every time

--- common/src/main/java/dev/tonimatas/packetfixer/common/Config.java
+++ common/src/main/java/dev/tonimatas/packetfixer/common/Config.java
@@ -6,7 +6,17 @@ import java.nio.file.Files;
 import java.util.Properties;
 
 public class Config {
-    private static Properties properties = null;
+    private static boolean allSizesUnlimited = true;
+    private static long nbtMaxSize = 2097152;
+    private static int packetSize = 1048576;
+    private static int decoderSize = 8388608;
+    private static int varInt21 = 3;
+    private static int varInt = 5;
+    private static int varLong = 10;
+    private static int stringSize = 32767;
+    private static int chunkPacketData = 2097152;
+    private static int timeout = 90;
+    private static boolean forceUnlimitedNbtEnabled = false;
 
     @SuppressWarnings("ResultOfMethodCallIgnored")
     public static void runProperties() {
@@ -18,107 +28,80 @@ public class Config {
             }
 
             File propertiesFile = new File(configFolder, "packetfixer.properties");
-            properties = new Properties();
+            Properties properties = new Properties();
 
             if (!propertiesFile.exists()) {
                 propertiesFile.createNewFile();
-                
-                properties.setProperty("allSizesUnlimited", "true");
-                properties.setProperty("nbtMaxSize", Long.toString(2097152));
-                properties.setProperty("packetSize", Integer.toString(1048576));
-                properties.setProperty("decoderSize", Integer.toString(8388608));
-                properties.setProperty("varInt21", Integer.toString(3));
-                properties.setProperty("varInt", Integer.toString(5));
-                properties.setProperty("varLong", Integer.toString(10));
-                properties.setProperty("stringSize", Integer.toString(32767));
-                checkVariable("chunkPacketData", Integer.toString(2097152));
-                properties.setProperty("timeout", Integer.toString(90));
-                properties.setProperty("forceUnlimitedNbtEnabled", Boolean.toString(false));
-
-                save(propertiesFile);
             }
 
             properties.load(Files.newInputStream(propertiesFile.toPath()));
 
-            checkVariable("allSizesUnlimited", "true");
-            checkVariable("nbtMaxSize", Long.toString(2097152));
-            checkVariable("packetSize", Integer.toString(1048576));
-            checkVariable("decoderSize", Integer.toString(8388608));
-            checkVariable("varInt21", Integer.toString(3));
-            checkVariable("varInt", Integer.toString(5));
-            checkVariable("varLong", Integer.toString(10));
-            checkVariable("stringSize", Integer.toString(32767));
-            checkVariable("chunkPacketData", Integer.toString(2097152));
-            checkVariable("timeout", Integer.toString(90));
-            checkVariable("forceUnlimitedNbtEnabled", Boolean.toString(false));
-            save(propertiesFile);
+            allSizesUnlimited = Boolean.parseBoolean(checkVariable(properties, "allSizesUnlimited", String.valueOf(allSizesUnlimited)));
+            nbtMaxSize = Long.parseLong(checkVariable(properties, "nbtMaxSize", String.valueOf(nbtMaxSize)));
+            packetSize = Integer.parseInt(checkVariable(properties, "packetSize", Integer.toString(packetSize)));
+            decoderSize = Integer.parseInt(checkVariable(properties, "decoderSize", Integer.toString(decoderSize)));
+            varInt21 = Integer.parseInt(checkVariable(properties, "varInt21", Integer.toString(varInt21)));
+            varInt = Integer.parseInt(checkVariable(properties, "varInt", Integer.toString(varInt)));
+            varLong = Integer.parseInt(checkVariable(properties, "varLong", Integer.toString(varLong)));
+            stringSize = Integer.parseInt(checkVariable(properties, "stringSize", Integer.toString(stringSize)));
+            chunkPacketData = Integer.parseInt(checkVariable(properties, "chunkPacketData", Integer.toString(chunkPacketData)));
+            timeout = Integer.parseInt(checkVariable(properties, "timeout", Integer.toString(timeout)));
+            forceUnlimitedNbtEnabled = Boolean.parseBoolean(checkVariable(properties, "forceUnlimitedNbtEnabled", Boolean.toString(forceUnlimitedNbtEnabled)));
+            save(properties, propertiesFile);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private static boolean getUnlimitedPacketSize() {
-        if (properties == null) runProperties();
-        return Boolean.parseBoolean(properties.getProperty("allSizesUnlimited"));
-    }
-
     public static long getNbtMaxSize() {
-        if (properties == null) runProperties();
-        return getUnlimitedPacketSize() ? Long.MAX_VALUE : Long.parseLong(properties.getProperty("nbtMaxSize"));
+        return allSizesUnlimited ? Long.MAX_VALUE : nbtMaxSize;
     }
 
     public static int getPacketSize() {
-        if (properties == null) runProperties();
-        return getUnlimitedPacketSize() ? Integer.MAX_VALUE : Integer.parseInt(properties.getProperty("packetSize"));
+        return allSizesUnlimited ? Integer.MAX_VALUE : packetSize;
     }
 
     public static int getDecoderSize() {
-        if (properties == null) runProperties();
-        return getUnlimitedPacketSize() ? Integer.MAX_VALUE : Integer.parseInt(properties.getProperty("decoderSize"));
+        return allSizesUnlimited ? Integer.MAX_VALUE : decoderSize;
     }
 
     public static int getVarInt21Size() {
-        if (properties == null) runProperties();
-        return getUnlimitedPacketSize() ? Integer.MAX_VALUE : Integer.parseInt(properties.getProperty("varInt21"));
+        return allSizesUnlimited ? Integer.MAX_VALUE : varInt21;
     }
 
     public static int getVarIntSize() {
-        if (properties == null) runProperties();
-        return getUnlimitedPacketSize() ? (Integer.MAX_VALUE / 2 - 1) : Integer.parseInt(properties.getProperty("varInt"));
+        return allSizesUnlimited ? (Integer.MAX_VALUE / 2 - 1) : varInt;
     }
     
     public static int getVarLong() {
-        if (properties == null) runProperties();
-        return getUnlimitedPacketSize() ? (Integer.MAX_VALUE / 2 - 1) : Integer.parseInt(properties.getProperty("varLong"));
+        return allSizesUnlimited ? (Integer.MAX_VALUE / 2 - 1) : varLong;
     }
     
     public static int getStringSize() {
-        if (properties == null) runProperties();
-        return getUnlimitedPacketSize() ? 327670000 : Integer.parseInt(properties.getProperty("stringSize"));
+        return allSizesUnlimited ? 327670000 : stringSize;
     }
     
     public static int getChunkPacketData() {
-        if (properties == null) runProperties();
-        return getUnlimitedPacketSize() ? Integer.MAX_VALUE : Integer.parseInt(properties.getProperty("chunkPacketData"));
+        return allSizesUnlimited ? Integer.MAX_VALUE : chunkPacketData;
     }
     
     public static int getTimeout() {
-        if (properties == null) runProperties();
-        return Integer.parseInt(properties.getProperty("timeout"));
+        return timeout;
     }
     
     public static boolean isForceUnlimitedNbtEnabled() {
-        if (properties == null) runProperties();
-        return Boolean.parseBoolean(properties.getProperty("forceUnlimitedNbtEnabled"));
+        return forceUnlimitedNbtEnabled;
     }
 
-    private static void checkVariable(String variable, String defaultValue) {
+    private static String checkVariable(Properties properties, String variable, String defaultValue) {
         if (properties.getProperty(variable) == null) {
             properties.setProperty(variable, defaultValue);
         }
+        
+        return properties.getProperty(variable);
     }
 
-    private static void save(File propertiesFile) throws IOException {
+    private static void save(Properties properties, File propertiesFile) throws IOException {
         properties.store(Files.newOutputStream(propertiesFile.toPath()),
                 "Packet Fixer config file.\n" +
                         "Default values (minecraft default): nbtMaxSize 2097152, packetSize 1048576, decoderSize 8388608 and varInt21Size 3.\n" +
