d313b642575f6d2b1cb99a51a180178bb9bd0c24
tonimatasdev <79017964+TonimatasDEV@users.noreply.github.com>
2025-10-13 20:03:50 +0200

    chore(libs): Update to latest versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     // https://fabricmc.net/develop/
     id("fabric-loom") version "1.11-SNAPSHOT" apply false
     // https://projects.neoforged.net/neoforged/moddevgradle
-    id("net.neoforged.moddev") version "2.0.112" apply false
+    id("net.neoforged.moddev") version "2.0.115" apply false
 }
 
 tasks.register<DefaultTask>("export") {
