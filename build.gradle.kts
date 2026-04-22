5017b9515ad10848d0a8db17d876ab7c6ff366d4
tonimatasdev <79017964+TonimatasDEV@users.noreply.github.com>
2026-04-22 17:05:12 +0200

    chore: 1.21.11

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,11 @@
 plugins {
     // https://fabricmc.net/develop/
-    id("net.fabricmc.fabric-loom") version "1.15-SNAPSHOT" apply false
+    id("fabric-loom") version "1.14-SNAPSHOT" apply false
     // https://projects.neoforged.net/neoforged/moddevgradle
-    id("net.neoforged.moddev") version "2.0.141" apply false
+    id("net.neoforged.moddev") version "2.0.123" apply false
+}
+
+tasks.register<DefaultTask>("export") {
+    dependsOn(project(":fabric").tasks.named("remapJar"))
+    dependsOn(project(":neoforge").tasks.named("jar"))
 }
--- fabric/build.gradle.kts
+++ fabric/build.gradle.kts
@@ -1,17 +1,18 @@
 plugins {
     id("multiloader-loader")
-    id("net.fabricmc.fabric-loom")
+    id("fabric-loom")
 }
 
 val minecraftVersion: String by extra
-val fabricMinecraftVersionRange: String by extra
 val fabricLoaderVersion: String by extra
 val fabricVersion: String by extra
 
 dependencies {
     minecraft("com.mojang:minecraft:${minecraftVersion}")
-    implementation("net.fabricmc:fabric-loader:${fabricLoaderVersion}")
-    implementation("net.fabricmc.fabric-api:fabric-api:${fabricVersion}")
+    mappings(loom.officialMojangMappings())
+
+    modImplementation("net.fabricmc:fabric-loader:${fabricLoaderVersion}")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:${fabricVersion}")
 }
 
 loom {
